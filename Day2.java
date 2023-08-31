e7f36cf0d5b2f0dc84a81c8d8360720a94a05099
ArZie01 <104635385+ArZie01@users.noreply.github.com>
2023-08-31 11:36:55 +0800

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package skillcaptain;
+
+/**
+ *
+ * @author kurta
+ */
+public class Day2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+        // Initialize
+        int myint = 19;
+        char myChar = 'L';
+        double myDouble = 18.56;
+        float myFloat = 2.02f;
+        
+        // Print
+        System.out.println("Integer: " + myint);
+        System.out.println("Character: " + myChar);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Float: " + myFloat);
+    }
+}
